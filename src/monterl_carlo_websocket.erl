933109fe42488b3e1746f86e85e6f333011a43ba
nisbus <nisbus@gmail.com>
2012-11-02 00:19:53 +0000

    Fixed the startup and the procfile (thanks archaelus)

--- src/monterl_carlo_websocket.erl
+++ src/monterl_carlo_websocket.erl
@@ -45,8 +45,8 @@ start_phase(listen,_Type,_Args) ->
 			    {mimetypes, [{<<".js">>, [<<"text/javascript">>]}]}]},
 			 {'_', monterl_carlo_websocket_handler, []}
 		      ]}],  
-
-    cowboy:start_http(http, 100, [{port, config(http_port)}],[{dispatch, Dispatch}]).
+    cowboy:start_http(http, 100, [{port, config(http_port)}],[{dispatch, Dispatch}]),
+    ok.
 
 start_ok(_App, _Type, ok) -> ok;
 start_ok(_App, _Type, {error, {already_started, _App}}) -> ok;
